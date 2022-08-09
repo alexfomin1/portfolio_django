e11665fe50e36ea2e64707b7ba89b269643fd3cb
Ольга Сандыга <olgasandyga@MacBook-Air-Olga.local>
2022-08-09 18:35:10 +0300

    html template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-def homePageView(request):
-	return HttpResponse('Olya')
+
+class HomePageView(TemplateView):
+	template_name = 'home.html'
--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import django_heroku
 from pathlib import Path
+import gunicorn
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +58,7 @@ ROOT_URLCONF = 'portfolio_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
