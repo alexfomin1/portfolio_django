9bdc5bd699a1fa94ed9706f51fd7afeb14a1f437
Alex Fomin <me@fomin3.ru>
2022-08-10 17:28:47 +0300

    hosts

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-z4)rmg45xk2@(-zki%)@y)k#^#gj98*h(^zt4s_8&d(+c=&jva
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
