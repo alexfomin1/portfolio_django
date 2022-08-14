b3155cc62ff3f858b9655fc0f308c6e0a5558735
Alex Fomin <me@fomin3.ru>
2022-08-14 07:48:19 +0300

    css added

--- portfolio_django/settings.py
+++ portfolio_django/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'portfolio_django.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
