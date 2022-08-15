c96ba0b856bef5bdc71fa2d43dfab92714e36241
Alex Fomin <me@fomin3.ru>
2022-08-15 10:34:54 +0300

    grid design new

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,8 +2,10 @@ from django.db import models
 
 
 class Post(models.Model):
-	title = models.CharField(max_length=200)
-	text = models.TextField()
+	title = models.CharField(max_length=50)
+	body = models.TextField(max_length=300)
+	resource_name = models.CharField(max_length=40)
+	resource_url = models.URLField()
 
 	def __str__(self):
 		return self.title
